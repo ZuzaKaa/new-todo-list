057dbef57e2e63aa811f9cf61da0d84e86aec708
Zuzanna Kiljańska <zuza.ostrowska@gmail.com>
2020-07-03 14:58:21 +0200

    Updates

---
+++ precache-manifest.cf141e477b1becddab609db42da9c927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d660abb158454513cfae633002b8e6fd",
+    "url": "/new-todo-list/index.html"
+  },
+  {
+    "revision": "b57fb4160cf488496cc8",
+    "url": "/new-todo-list/static/css/main.6a81a415.chunk.css"
+  },
+  {
+    "revision": "3c497a74c70c8c74c27d",
+    "url": "/new-todo-list/static/js/2.dfb6ba00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-todo-list/static/js/2.dfb6ba00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57fb4160cf488496cc8",
+    "url": "/new-todo-list/static/js/main.e4a83569.chunk.js"
+  },
+  {
+    "revision": "ba1a0805b0bade15837a",
+    "url": "/new-todo-list/static/js/runtime-main.6ee5ce2e.js"
+  }
+]);
